f4c5af24123b7ecd019cba82a525805055dffb2c
Day Fisher <day.fisher@discordapp.com>
2021-08-18 15:16:59 -0700

    Misc small cleanup for #5 (#7)

--- pre_commit/editor.py
+++ pre_commit/editor.py
@@ -1,6 +1,7 @@
 import argparse
 import concurrent.futures
 import contextlib
+import platform
 import shlex
 import subprocess
 from pathlib import Path
@@ -32,7 +33,12 @@ COMMIT_MESSAGE_HEADER = """
 
 
 def should_run_concurrently(hook_stage: str) -> bool:
-    return hook_stage == 'commit' and _is_editor_script_configured() and _should_open_editor()
+    return (
+        hook_stage == 'commit' and
+        _is_editor_script_configured() and
+        _should_open_editor() and
+        platform.system != 'Windows'
+    )
 
 
 def run_concurrently(fun: Callable[..., T], *args: Any) -> T:
@@ -63,7 +69,8 @@ def should_clean_draft(hook_stage: str) -> bool:
         hook_stage == 'commit' and
         _is_editor_script_configured() and
         not _should_open_editor() and
-        COMMIT_MESSAGE_DRAFT_PATH.exists()
+        COMMIT_MESSAGE_DRAFT_PATH.exists() and
+        platform.system != 'Windows'
     )
 
 
@@ -168,6 +175,8 @@ def _edit_commit_message(template: str) -> None:
         COMMIT_MESSAGE_DRAFT_PATH.write_text(commit_draft + template)
     git_editor = _get_global_git_editor()  # Doesn't run in this repo, so the concurrency won't cause git lock errors.
     with monitor.trace('precommit.editor'):
+        # For some editors (e.g. vim), it's important that stdin be an interactive terminal.
+        # /dev/tty is a synonym for our process's controlling terminal.
         with open('/dev/tty') as stdin:
             subprocess.call(git_editor + [str(COMMIT_MESSAGE_DRAFT_PATH)], stdin=stdin)
 
